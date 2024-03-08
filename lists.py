cf032f7766a92426f28489082eeb0c9be319d8a5
Jamalshah <jimisk811@gmail.com>
2024-03-08 13:40:06 +0500

    Lists

---
+++ lists.py
@@ -0,0 +1,15 @@
+friends = ["Karen", "Kevin", "Jim", "Oscar", "Toby"] #you can put numbers, strings, bools as well
+print(friends)
+print(friends[0])
+print(friends[1])
+print(friends[2])
+print(friends[-1])
+print(friends[-2])
+print(friends[1:]) #from index one to onwards show
+print(friends[1:3]) #from one two three but no select three
+
+friends[1] = "Mike"
+print(friends[1])
+
+print(friends[:3])
+
