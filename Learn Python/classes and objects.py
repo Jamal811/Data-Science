37f819671bfda3f39fbf0e3f408f84e60b7b42e7
Jamalshah <jimisk811@gmail.com>
2024-03-13 11:01:44 +0500

    classes and objects

---
+++ Learn Python/classes and objects.py
@@ -0,0 +1,6 @@
+from studentclass import Student
+
+student1 = Student("Jamal", "CS", "3.2", False)
+student2 = Student("Kamal", "SE", "2", True)
+print(student1.major)
+print(student2.is_on_probation)
