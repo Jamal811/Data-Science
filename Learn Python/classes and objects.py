8012070fea4976fceef6de12c9896d470fb7653f
Jamalshah <jimisk811@gmail.com>
2024-03-13 11:44:26 +0500

    updated the class to add the function inside the class

--- Learn Python/classes and objects.py
+++ Learn Python/classes and objects.py
@@ -1,6 +1,6 @@
 from studentclass import Student
 
-student1 = Student("Jamal", "CS", "3.2", False)
-student2 = Student("Kamal", "SE", "2", True)
+student1 = Student("Jamal", "CS", 3.6, False)
+student2 = Student("Kamal", "SE", 2, True)
 print(student1.major)
-print(student2.is_on_probation)
+print(student1.on_honor_roll())
