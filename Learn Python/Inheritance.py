c98bb034b11de49ff99e18f11f87bf612f09f24d
Jamalshah <jimisk811@gmail.com>
2024-03-13 12:03:10 +0500

    Inheritance of classes

---
+++ Learn Python/Inheritance.py
@@ -0,0 +1,8 @@
+from ChefClass import Chef
+from ChineeseChef import ChineeseChef
+
+myChef = Chef()
+myChef.make_special_dish()
+
+myChoneeseChef = ChineeseChef()
+myChoneeseChef.make_special_dish()
